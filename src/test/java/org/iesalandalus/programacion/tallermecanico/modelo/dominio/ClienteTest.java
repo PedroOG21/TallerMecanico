8b1bc22b0d38d09260de07afb5a90b089a6311bc
PedroOG21 <portizgalera@gmail.com>
2024-02-12 09:07:56 +0100

    Clase Cliente creada.

--- src/test/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/ClienteTest.java
+++ src/test/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/ClienteTest.java
@@ -1,5 +1,6 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.dominio;
 
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.Cliente;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
